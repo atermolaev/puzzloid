9d6f4edcd2b78767e8688b4b262dc330593f72e5
Anton Termolaev <a.termolaev@mail.ru>
2025-12-26 12:24:40 +0300

    Add init function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-Object.prototype.inherit = function(Parent){
-    const F = function() { }
-    F.prototype = Parent.prototype
-    this.prototype = new F()
-    this.prototype.constructor = this
-    this.superclass = Parent.prototype
-}
+Object.prototype.inherit = function (Parent) {
+  const F = function () {};
+  F.prototype = Parent.prototype;
+  this.prototype = new F();
+  this.prototype.constructor = this;
+  this.superclass = Parent.prototype;
+};
 
-function Main() {
+function Main() {}
 
-}
+Main.prototype.init = function () {
+  var draw = SVG().addTo(".container").size(300, 300);
+  var rect = draw.rect(100, 100);
+  rect.attr({ fill: "#f06" });
+  rect.click(function () {
+    this.fill({ color: "#b32020ff" });
+  });
+};
 
-Main.prototype.method = function(param){
-    return `param: ${param}`;
-}
-
-function Box() {
-
-}
+function Box() {}
 
 Box.inherit(Main);
